e7963ade30e2ec96600bb31102f718492bd8edb5
Xiaowei Xue <xiaowei.xue@shopee.com>
2022-12-29 18:17:30 +0800

    chore: use pnpm

--- server-side-render-only/remoteServer/SharedComponent.js
+++ server-side-render-only/remoteServer/SharedComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const SharedComponent = () => <div>Shared Component2222</div>;
+const SharedComponent = () => <div>Shared Component</div>;
 
 export default SharedComponent;
